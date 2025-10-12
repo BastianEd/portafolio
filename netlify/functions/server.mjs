e995a2ed57969b7ba47d00071fd05b29570e7273
BastianEd <ba.rubiom@duocuc.cl>
2025-10-11 22:14:47 -0300

    feat: update Netlify configuration and build settings

--- netlify/functions/server.mjs
+++
@@ -1,8 +0,0 @@
-// netlify/functions/server.js
-
-// Usamos require() en lugar de import
-const { createRequestHandler } = require("@react-router/node");
-const build = require("../../build/server/index.js");
-
-// La exportación del handler sigue siendo la misma
-exports.handler = createRequestHandler(build);
